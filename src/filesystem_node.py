d6b67ca2f7d853994b3fed6631d5d7e26e98846e
greensteamengine <tomi123v@gmail.com>
2024-07-02 18:44:08 +0200

    Fixed logic of building fs node tree

--- src/filesystem_node.py
+++ src/filesystem_node.py
@@ -1,5 +1,4 @@
 import os
-from typing import Self
 from bisect import insort_left
 
 
@@ -17,14 +16,19 @@ class FileSystemNode(object):
         root_node = FileSystemNode(root_path, None)
 
         next_max_depth = None if max_depth is None else max_depth - 1
-        if next_max_depth > 0:
+        #print("####", root_node.type)
+        if root_node.type == "dir" and (next_max_depth is None or next_max_depth > 0):
             try:
                 for entry in os.scandir(root_path):
                     if should_include(entry.path):
-                        new_child_node = FileSystemNode(entry.path, root_node)
+
+                        #new_child_node = FileSystemNode(entry.path, root_node)
+                        #root_node.add_child(new_child_node)
+                        #if entry.is_dir():
+                        print(f"###################### {entry.path} {next_max_depth}")
+                        new_child_node = FileSystemNode.build_directory_tree(entry.path, next_max_depth, use_filter)
+                        new_child_node.parent = root_node
                         root_node.add_child(new_child_node)
-                        if entry.is_dir():
-                            new_child_node.build_directory_tree(entry.path, next_max_depth, use_filter)
 
             except PermissionError:
                 # Handle the case where the program does not have permissions to access a directory
@@ -53,12 +57,23 @@ class FileSystemNode(object):
     def parent(self):
         return self.__parent
 
+    @parent.setter
+    def parent(self, parent):
+        self.__parent = parent
+
     @property
     def children(self):
         return self.__children
 
-    def add_child(self, child_node: Self):
+    @property
+    def type(self):
+        return self.__type
+
+    def add_child(self, child_node):
         insort_left(self.__children, child_node)
+        #for c in self.__children:
+        #    print(f"<{c.name}>", end=" ")
+        #print("")
 
     @property
     def name(self):
@@ -72,12 +87,21 @@ class FileSystemNode(object):
         stack = [(self, 0)]
         while stack:
             current_node, node_depth = stack.pop()
+            #print(">>>", current_node.name, len(current_node.children))
+            #for n,d in stack:
+            #    print(n.name, d, end=" ")
+            #print("")
+
             if include_depth:
+                #print(current_node.name, node_depth)
                 yield current_node, node_depth
             else:
                 yield current_node
-            child_data = [(node, node_depth+1) for node in current_node.children]
+
+            child_data = [(node, node_depth + 1) for node in current_node.children]
+            #print("PPPPP", len(current_node.children))
             stack.extend(reversed(child_data))
+
 
     def calculate_tree_node_sizes(self):
         if self.__type == 'dir':
