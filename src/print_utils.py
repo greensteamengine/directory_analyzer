51807e4020fd17dc6717c55b3d541298a1ec48bf
greensteamengine <tomi123v@gmail.com>
2024-07-02 14:40:27 +0200

    Function handling print formatting of directory nodes

---
+++ src/print_utils.py
@@ -0,0 +1,6 @@
+def print_directory_structure(root_node):
+    for node, depth in root_node.subtree_iterator(include_depth=True):
+        print_sequence = "   " * depth + f"{node.name}"
+        if node.size is not None:
+            print_sequence += f" [{node.size}B]"
+        print(print_sequence)
